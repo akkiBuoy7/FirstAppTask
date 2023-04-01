2a5de12c4900358f025f6254c2b095e810311b88
akkiB <akash3350saha@gmail.com>
2023-04-01 11:44:17 +0530

    skip feature

--- lib/ui/bottom_nav_screens/bottom_nav_detail_screens/video_player/advance_overlay_layout.dart
+++ lib/ui/bottom_nav_screens/bottom_nav_detail_screens/video_player/advance_overlay_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class AdvanceOverlayWidget extends StatelessWidget {
+class AdvanceOverlayWidget extends StatefulWidget {
   final VideoPlayerController videoPlayerController;
   final VoidCallback onClickedFullScreen;
   static var allSpeed = [0.25, 0.5, 1, 1.5, 2, 3];
@@ -13,82 +13,124 @@ class AdvanceOverlayWidget extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<AdvanceOverlayWidget> createState() => _AdvanceOverlayWidgetState();
+}
+
+class _AdvanceOverlayWidgetState extends State<AdvanceOverlayWidget> {
+  bool showOvelay = true;
+
+  @override
   Widget build(BuildContext context) {
-    final isMuted = videoPlayerController.value.volume == 0;
+    final isMuted = widget.videoPlayerController.value.volume == 0;
 
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          videoPlayerController.value.isPlaying
-              ? videoPlayerController.pause()
-              : videoPlayerController.play();
-        },
-        child: Stack(
-          children: [
-            buildPlay(),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 5,
-                    ),
-                    ValueListenableBuilder(
-                        valueListenable: videoPlayerController,
-                        builder: (context, VideoPlayerValue value, child) {
-                          return Text(
-                            _videoDuration(value.position),
-                            style: TextStyle(fontSize: 10, color: Colors.white),
-                          );
-                        }),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(child: buildIndicator()),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      _videoDuration(videoPlayerController.value.duration),
-                      style: TextStyle(fontSize: 10, color: Colors.white),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                  ],
-                )),
-            Positioned(
-                top: 5,
-                right: 5,
-                child: GestureDetector(
-                    onTap: onClickedFullScreen,
-                    child: Icon(
-                      Icons.fullscreen,
-                      color: Colors.white,
-                    ))),
-            Positioned(
-                left: 5,
-                child: Row(
-                  children: [
-                    buildSpeed(),
-                    SizedBox(),
-                    buildVolume(isMuted),
-                  ],
-                )),
-          ],
-        ),
-      ),
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            setState(() {
+              showOvelay = !showOvelay;
+              print("##############Show Overlay${showOvelay}");
+            });
+          },
+          child: showHideOverlay(showOvelay, isMuted)),
     );
+  }
+
+  Widget showHideOverlay(bool showOverlay, bool isMuted) {
+    if (showOvelay) {
+      return Stack(
+        children: [
+          mainContainer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    onTap: () {
+                      backward2Sec();
+                    },
+                    child: Icon(
+                      Icons.replay_5,
+                      color: Colors.white,
+                    )),
+              ),
+              buildPlay(),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    onTap: () {
+                      forward2Sec();
+                    },
+                    child: Icon(
+                      Icons.forward_5,
+                      color: Colors.white,
+                    )),
+              )
+            ],
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 5,
+                  ),
+                  ValueListenableBuilder(
+                      valueListenable: widget.videoPlayerController,
+                      builder: (context, VideoPlayerValue value, child) {
+                        return Text(
+                          _videoDuration(value.position),
+                          style: TextStyle(fontSize: 10, color: Colors.white),
+                        );
+                      }),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(child: buildIndicator()),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    _videoDuration(widget.videoPlayerController.value.duration),
+                    style: TextStyle(fontSize: 10, color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                ],
+              )),
+          Positioned(
+              top: 5,
+              right: 5,
+              child: GestureDetector(
+                  onTap: widget.onClickedFullScreen,
+                  child: Icon(
+                    Icons.fullscreen,
+                    color: Colors.white,
+                  ))),
+          Positioned(
+              left: 5,
+              child: Row(
+                children: [
+                  buildSpeed(),
+                  SizedBox(),
+                  buildVolume(isMuted),
+                ],
+              )),
+        ],
+      );
+    }
+    return Container();
   }
 
   Widget buildIndicator() {
     return SizedBox(
         height: 10,
-        child: VideoProgressIndicator(videoPlayerController,
+        child: VideoProgressIndicator(widget.videoPlayerController,
             allowScrubbing: true));
   }
 
@@ -96,23 +138,55 @@ class AdvanceOverlayWidget extends StatelessWidget {
     return IconButton(
       color: Colors.white,
       onPressed: () {
-        videoPlayerController.setVolume(isMuted ? 1 : 0);
+        widget.videoPlayerController.setVolume(isMuted ? 1 : 0);
       },
       icon: Icon(isMuted ? Icons.volume_mute : Icons.volume_up),
     );
   }
 
   Widget buildPlay() {
-    return videoPlayerController.value.isPlaying
-        ? Container()
+    return widget.videoPlayerController.value.isPlaying
+        ? GestureDetector(
+            onTap: () {
+              if (widget.videoPlayerController.value.isPlaying) {
+                widget.videoPlayerController.pause();
+              } else {
+                widget.videoPlayerController.play();
+              }
+            },
+            child: Center(
+              child: Icon(
+                Icons.pause_circle,
+                color: Colors.white,
+                size: 70,
+              ),
+            ),
+          )
+        : GestureDetector(
+            onTap: () {
+              if (widget.videoPlayerController.value.isPlaying) {
+                widget.videoPlayerController.pause();
+              } else {
+                widget.videoPlayerController.play();
+              }
+            },
+            child: Center(
+              child: Icon(
+                Icons.play_circle,
+                color: Colors.white,
+                size: 70,
+              ),
+            ),
+          );
+  }
+
+  Widget mainContainer() {
+    return widget.videoPlayerController.value.isPlaying
+        ? Container(
+            alignment: Alignment.center,
+          )
         : Container(
             alignment: Alignment.center,
-            child: Icon(
-              Icons.play_circle,
-              color: Colors.white,
-              size: 70,
-            ),
-            color: Colors.black26,
           );
   }
 
@@ -128,12 +202,12 @@ class AdvanceOverlayWidget extends StatelessWidget {
   Widget buildSpeed() {
     return PopupMenuButton(
       color: Colors.black,
-      initialValue: videoPlayerController.value.playbackSpeed,
+      initialValue: widget.videoPlayerController.value.playbackSpeed,
       tooltip: "speed",
       onSelected: (value) {
-        videoPlayerController.setPlaybackSpeed(value.toDouble());
+        widget.videoPlayerController.setPlaybackSpeed(value.toDouble());
       },
-      itemBuilder: (context) => allSpeed
+      itemBuilder: (context) => AdvanceOverlayWidget.allSpeed
           .map((speed) => PopupMenuItem(
               value: speed,
               child: Text(
@@ -148,11 +222,24 @@ class AdvanceOverlayWidget extends StatelessWidget {
             color: Colors.white,
           ),
           Text(
-            "${videoPlayerController.value.playbackSpeed}",
+            "${widget.videoPlayerController.value.playbackSpeed}",
             style: TextStyle(color: Colors.white),
           )
         ],
       ),
     );
   }
+
+  Future goToPosition(
+      Duration Function(Duration currentPosition) builder) async {
+    final currentPosition = await widget.videoPlayerController.position;
+    final newPosition = builder(currentPosition!);
+    await widget.videoPlayerController.seekTo(newPosition);
+  }
+
+  Future forward2Sec() async =>
+      goToPosition((currentPosition) => currentPosition + Duration(seconds: 2));
+
+  Future backward2Sec() async =>
+      goToPosition((currentPosition) => currentPosition - Duration(seconds: 2));
 }
